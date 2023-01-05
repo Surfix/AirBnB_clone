f731f55fcd5d3a8c577afdf5a4fb78b00f955e02
Naheem Adebisi <adebisi.naheem@gmail.com>
2023-01-05 10:19:12 +0000

    3. BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,32 @@
 #!/usr/bin/python3
-"""This is the Base Model For All Models."""
 
-from datetime import datetime as dtime
-from uuid import uuid4
-import models
+import uuid
+import datetime
 
+""" Parent class to all classes of the AirBnB clone project """
 
-class BaseModel:
-    """This Model Implement All common attribute for other model."""
-
-    def __init__(self, id, created_at, updated_at):
-        """Initialize the BaseModel."""
-       self.id = str(uuid4())
-       self.created_at = dtime.now()
-       self.updated_at() = dtime.now()
+class BaseModel():
+    """Base Model class that other classes inherit from"""
+    def __init__(self):
+        """initializes object of the BaseModel"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        """Return the string representation of BaseModel"""
-        return f'[{self.__class__.__name__}] ({self.id}) (self.__dict__)'
+        """Return the string representation of the BaseModel"""
+        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
-        """Update the BaseModel Data"""
-        self.updated_at = dtime,now()
+        """Update the Basemodel object's updated_at attribute"""
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        """Return the dictionary representation of BaseModel."""
-        attr = self.__dict__.copy()
-        attr['__class__'] = self.__class__.__name__
-        if not type(attr['created_at']) is str:
-            attr['created_at'] = attr['created_at'].isoformat()
-        if not type(attr['updated_at']) is str:
-            attr['updated_at'] = attr['updated_at'].isoformat()
-        return attr
+        """Return the dictionary representation of the BaseModel object"""
+        ins_dict = self.__dict__
+        ins_dict["__class__"] = self.__class__.__name__
+        if type(ins_dict["created_at"]) is not str:
+            ins_dict["created_at"] = ins_dict["created_at"].isoformat()
+        if type(ins_dict["updated_at"]) is not str:
+            ins_dict["updated_at"] = ins_dict["updated_at"].isoformat()
+        return ins_dict
