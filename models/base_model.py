4df45c906cac01ecb33f5c6dc359421f7b9efe40
Naheem Adebisi <adebisi.naheem@gmail.com>
2023-01-06 18:41:56 +0000

    4. Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,28 @@ import datetime
 
 class BaseModel():
     """Base Model class that other classes inherit from"""
-    def __init__(self):
-        """initializes object of the BaseModel"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, val in kwargs.items():
+                if key == "created_at":
+                    val = datetime.datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, val)
+                elif key == 'updated_at':
+                    val = datetime.datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, val)
+                elif key == '__class__':
+                    pass
+                else:
+                    setattr(self, key, val)
+        else:
+            """initializes object of the BaseModel"""
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """Return the string representation of the BaseModel"""
-        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
+        return ('[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         """Update the Basemodel object's updated_at attribute"""
